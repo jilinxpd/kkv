c3419e19dc5382d3a4f358d80f18babfa0d0f5e9
jilinxpd <jilinxpd@gmail.com>
2014-04-14 19:48:33 +0800

    implement clients for kkv.

--- fs.h
+++
@@ -1,9 +0,0 @@
-/*
- * In-Kernel Key/Value Store.
- *
- * Copyright (C) 2013 jilinxpd.
- *
- * This file is released under the GPL.
- */
-
-extern struct file_system_type kkv_fs_type;
--- hash.h
+++
@@ -1,9 +0,0 @@
-/*
- * In-Kernel Key/Value Store.
- *
- * Copyright (C) 2013 jilinxpd.
- *
- * This file is released under the GPL.
- */
-
-uint32_t hash(const void *key, size_t length, const uint32_t initval);
